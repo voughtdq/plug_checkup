e786178d83320276241643c456439e8fa616ad3c
Douglas Vought <doug@vought.co>
2024-07-17 18:40:43 -0400

    Allow setting the server port, don't crash outside of project

--- .iex.exs
+++ .iex.exs
@@ -1,59 +1,80 @@
 alias PlugCheckup.Check
 alias PlugCheckup.Options
+require Logger
 
-defmodule Health.RandomCheck do
-  def call do
-    probability = :rand.uniform()
+in_mix_project? =
+  try do
+    Mix.Project.get()
+    true
+  rescue
+    _ -> false
+  catch
+    _, _ -> false
+  end
 
-    cond do
-      probability < 0.5 -> :ok
-      probability < 0.6 -> {:error, "Error"}
-      probability < 0.7 -> raise(RuntimeError, message: "Exception")
-      probability < 0.8 -> exit(:boom)
-      probability < 0.9 -> throw(:ball)
-      true -> :timer.sleep(2000)
+unless in_mix_project? do
+  Logger.warning(fn -> 
+    "Not starting PlugCheckup test server, invoke iex with `-S mix` to start." 
+  end)
+end
+
+if in_mix_project? do
+  defmodule Health.RandomCheck do
+    def call do
+      probability = :rand.uniform()
+
+      cond do
+        probability < 0.5 -> :ok
+        probability < 0.6 -> {:error, "Error"}
+        probability < 0.7 -> raise(RuntimeError, message: "Exception")
+        probability < 0.8 -> exit(:boom)
+        probability < 0.9 -> throw(:ball)
+        true -> :timer.sleep(2000)
+      end
     end
   end
-end
 
-defmodule MyRouter do
-  use Plug.Router
+  defmodule MyRouter do
+    use Plug.Router
 
-  plug(:match)
-  plug(:dispatch)
+    plug(:match)
+    plug(:dispatch)
 
-  self_checks = [
-    %Check{name: "random1", module: Health.RandomCheck, function: :call},
-    %Check{name: "random2", module: Health.RandomCheck, function: :call}
-  ]
+    self_checks = [
+      %Check{name: "random1", module: Health.RandomCheck, function: :call},
+      %Check{name: "random2", module: Health.RandomCheck, function: :call}
+    ]
 
-  forward(
-    "/selfhealth",
-    to: PlugCheckup,
-    init_opts: Options.new(json_encoder: Jason, checks: self_checks, timeout: 1000)
-  )
+    forward(
+      "/selfhealth",
+      to: PlugCheckup,
+      init_opts: Options.new(json_encoder: Jason, checks: self_checks, timeout: 1000)
+    )
 
-  deps_checks = [
-    %Check{name: "random1", module: Health.RandomCheck, function: :call},
-    %Check{name: "random2", module: Health.RandomCheck, function: :call}
-  ]
+    deps_checks = [
+      %Check{name: "random1", module: Health.RandomCheck, function: :call},
+      %Check{name: "random2", module: Health.RandomCheck, function: :call}
+    ]
 
-  forward(
-    "/dependencyhealth",
-    to: PlugCheckup,
-    init_opts:
-      Options.new(
-        json_encoder: Jason,
-        checks: deps_checks,
-        timeout: 3000,
-        pretty: false,
-        time_unit: :millisecond
-      )
-  )
+    forward(
+      "/dependencyhealth",
+      to: PlugCheckup,
+      init_opts:
+        Options.new(
+          json_encoder: Jason,
+          checks: deps_checks,
+          timeout: 3000,
+          pretty: false,
+          time_unit: :millisecond
+        )
+    )
 
-  match _ do
-    send_resp(conn, 404, "oops")
+    match _ do
+      send_resp(conn, 404, "oops")
+    end
   end
-end
 
-{:ok, _} = Plug.Adapters.Cowboy.http(MyRouter, [])
+  port = String.to_integer(System.get_env("TEST_SERVER_PORT", "4000"))
+  server = [{Plug.Cowboy, plug: MyRouter, scheme: :http, options: [port: port]}]
+  {:ok, _} = Supervisor.start_link(server, strategy: :one_for_one)
+end
